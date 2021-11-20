94f51b792cbfb4e8d25bd7a672e6e5d56f0e9204
Capucine Daveine <capucine_daveine@ens.univ-artois.fr>
2021-11-20 13:51:12 +0100

    :sparkles: poketudiant add xp and pv_current, enseignantdresseur tab poketudiant

--- java/app/src/main/java/Poke_University/Poketudiant.java
+++ java/app/src/main/java/Poke_University/Poketudiant.java
@@ -3,13 +3,16 @@ package Poke_University;
 import java.util.HashMap;
 
 public abstract class Poketudiant {
+    private String nom;
     private Type type;
     private boolean catchable;
     private Poketudiant evolution;
     private int attack;
     private int defense;
     private int PV_max;
+    private int PV_current;
     private Attack[] attacks;
+    private int xp;
 
     public Poketudiant(){
         
